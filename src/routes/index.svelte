89ed0893d6f2f7f0fe3e12def019a1086d01290e
Arthur Lechte <arthlechte@gmail.com>
2022-03-19 10:32:34 +0900

    delete: everything, start from scratch

--- src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import AboutMe from '../components/AboutMe.svelte';
-	import Footer from '../components/Footer.svelte';
-	import RecentPosts from '../components/RecentPosts.svelte';
-	import NavbarLayout from '../components/NavbarLayout.svelte';
-</script>
-
-<AboutMe />
-<RecentPosts />
